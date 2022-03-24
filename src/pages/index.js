6a462765fc0b831eac6b93d7dbfd337c8c25fe99
allpavel <allpavel@yandex.ru>
2022-03-24 20:03:08 +0300

    added a whyUs component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import Layout from "../components/Layout/Layout";
 import Hero from "../components/Hero/Hero";
 import Services from "../components/Services/Services";
+import WhyUs from "../components/WhyUs/WhyUs";
 
 export default function Home() {
     return (
         <Layout>
             <Hero />
             <Services />
-            <div>Hello world!</div>
+            <WhyUs />
         </Layout>
     );
 }
