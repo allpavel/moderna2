5e15716f17da09b0898c15ea53cf7ad0a4d8dba6
allpavel <allpavel@yandex.ru>
2022-04-02 22:25:01 +0300

    added the testimonials component to the about page

---
+++ src/components/aboutPage/Testimonials/Testimonials.js
@@ -0,0 +1,159 @@
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+import styled from "styled-components";
+import SwiperCore, { Pagination } from "swiper/core";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+
+SwiperCore.use([Pagination]);
+
+const Wrapper = styled.section`
+    padding: 3.75rem 0;
+`;
+
+const Container = styled.section`
+    max-width: var(--max-width-desktop);
+    margin: 0 auto;
+`;
+
+const Title = styled.div`
+    text-align: center;
+    padding-bottom: 1.875rem;
+
+    h2 {
+        font-size: 1.75rem;
+        font-weight: 400;
+        margin-top: 0;
+        margin-bottom: 1.25rem;
+        padding-bottom: 1.25rem;
+        position: relative;
+    }
+
+    h2:before {
+        content: "";
+        position: absolute;
+        display: block;
+        width: 7.5rem;
+        height: 1px;
+        background-color: #ddd;
+        bottom: 1px;
+        left: calc(50% - 3.75rem);
+    }
+
+    h2:after {
+        content: "";
+        position: absolute;
+        display: block;
+        width: 2.5rem;
+        height: 3px;
+        background-color: var(--hero-color);
+        bottom: 0;
+        left: calc(50% - 1.25rem);
+    }
+
+    p {
+        margin: 0;
+        line-height: 1.5rem;
+    }
+`;
+
+const TestimonialCarousel = styled.div`
+    img {
+        width: 120px;
+        height: auto;
+        border-radius: 50%;
+        border: 4px solid #fff;
+        margin: 0 auto;
+    }
+    h3 {
+        margin: 10px 0 5px 0;
+        color: black;
+    }
+
+    p {
+        color: #999;
+        margin-bottom: 15px;
+    }
+
+    p:last-child {
+        color: #000;
+        font-style: italic;
+        margin-bottom: 2rem;
+    }
+
+    .swiper-pagination-bullets {
+        bottom: 2px;
+    }
+`;
+
+const Testimonials = () => {
+    return (
+        <Wrapper>
+            <Container>
+                <Title>
+                    <h2>Testimonials</h2>
+                    <p>
+                        Rule years in after isn't fourth thing them likeness.
+                        Face you spirit air place fish i cattle for wherein
+                        isn't them. Shall image darkness and meat make together
+                        upon him don't form may his beginning fill void. Open
+                        creeping, image.
+                    </p>
+                </Title>
+                <TestimonialCarousel>
+                    <Swiper
+                        slidesPerView={1}
+                        loop={true}
+                        pagination={{ clickable: true }}
+                        modules={{ Pagination }}
+                    >
+                        <SwiperSlide>
+                            <StaticImage
+                                src="../../../images/testimonials/testimonials-1.jpg"
+                                alt="CEO"
+                            />
+                            <h3>Saul Goodman</h3>
+                            <p>CEO &#38; Founder</p>
+                            <p>
+                                Sea also sea winged sea whales likeness them
+                                firmament. Form creeping without earth life i,
+                                that hath. Dry abundantly. You'll unto there
+                                great won't it winged lesser. Form signs sixth.
+                            </p>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <StaticImage
+                                src="../../../images/testimonials/testimonials-2.jpg"
+                                alt="CEO"
+                            />
+                            <h3>Saul Goodman</h3>
+                            <p>CEO &#38; Founder</p>
+                            <p>
+                                Multiply every hath bring whose set is of. There
+                                living be Earth the place gathering fill lesser
+                                itself tree all and whose all sea dry won't fly
+                                fly unto moved.
+                            </p>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <StaticImage
+                                src="../../../images/testimonials/testimonials-3.jpg"
+                                alt="CEO"
+                            />
+                            <h3>Saul Goodman</h3>
+                            <p>CEO &#38; Founder</p>
+                            <p>
+                                He forth thing his fifth morning fifth beast god
+                                gathered. Second. Face subdue day made. The
+                                subdue years dry lights let cattle tree make,
+                                abundantly seasons Given be he place.
+                            </p>
+                        </SwiperSlide>
+                    </Swiper>
+                </TestimonialCarousel>
+            </Container>
+        </Wrapper>
+    );
+};
+
+export default Testimonials;
