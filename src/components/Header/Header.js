3b1e427a98db3ab4cbcc195981797734a111430f
allpavel <74268211+allpavel@users.noreply.github.com>
2022-04-12 15:08:31 +0300

    started doing a mobile version of the navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { useLocation } from "@reach/router";
+import { MdMenu, MdClose } from "react-icons/md";
 import Logo from "../Logo/Logo";
 import Navbar from "../Navbar/Navbar";
+import MobileMenu from "../MobileMenu/MobileMenu";
 
 const IndexHeader = css`
     background-color: ${({ header }) => (header ? "rgba(30, 67, 86, 0.8)" : "transparent")};
@@ -19,7 +21,7 @@ const Wrapper = styled.header`
     right: 0;
     left: 0;
     z-index: 1000;
-    ${({ location }) => location.pathname === '/' ? IndexHeader : NormalHeader}
+    ${({ location }) => (location.pathname === "/" ? IndexHeader : NormalHeader)}
     transition: all 0.5s ease;
 `;
 
@@ -28,13 +30,26 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    position: relative;
     height: 100%;
     padding: 0 0.75rem;
     margin: 0 auto;
+
+    @media screen and (max-width: 1500px) {
+        max-width: 1000px;
+    }
+    @media screen and (max-width: 1280px) {
+        max-width: 900px;
+    }
+
+    @media screen and (min-width: 769px) {
+        width: 100vw;
+    }
 `;
 
 const Header = () => {
     const [header, setHeader] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
     const location = useLocation();
 
     const changeHeaderBackground = () => {
@@ -52,6 +67,7 @@ const Header = () => {
             <Container>
                 <Logo />
                 <Navbar />
+                <MobileMenu isOpen={isOpen} />
             </Container>
         </Wrapper>
     );
---
+++ src/components/MobileMenu/MobileMenu.js
@@ -0,0 +1,104 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "gatsby";
+
+const Wrapper = styled.nav`
+    opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
+    height: ${({ isOpen }) => (isOpen ? "100vh" : "0")};
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: start;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(17, 38, 48, 0.95);
+    overflow: hidden;
+    z-index: 1000;
+    transition: height 0.4s ease;
+
+    @media screen and (min-width: 769px) {
+        display: none;
+    }
+
+    ul {
+        position: absolute;
+        top: 3.5rem;
+        bottom: 1rem;
+        right: 1rem;
+        left: 1rem;
+        padding: 10px 0;
+        background-color: #fff;
+        overflow-y: auto;
+    }
+
+    li {
+        list-style: none;
+        text-align: center;
+        margin: 1.5rem 0;
+    }
+
+    li a {
+        width: 100%;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 1.2rem;
+        color: #000;
+        transition: all 0.5s ease;
+
+        &:hover {
+            color: var(--main-color);
+        }
+    }
+
+    .active {
+        color: var(--main-color);
+    }
+`;
+
+const Dropdown = styled.div``;
+
+const Arrow = styled.div``;
+
+const MobileMenu = ({ isOpen }) => {
+    return (
+        <Wrapper isOpen={isOpen}>
+            <ul>
+                <li>
+                    <Link to="/" activeClassName="active">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about" activeClassName="active">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/services" activeClassName="active">
+                        Services
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link to="/team">Team</Link>
+                </li>
+                <li>
+                    <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                    <Link to="/contact" activeClassName="active">
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+        </Wrapper>
+    );
+};
+
+export default MobileMenu;
