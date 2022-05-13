61c8654193baa43fc2617a492720996a94ba6493
allpavel <allpavel@yandex.ru>
2022-05-13 21:50:42 +0300

    added a blog template

--- src/hooks/useGetPath.js
+++ src/hooks/useGetPath.js
@@ -2,8 +2,8 @@ import { useLocation } from "@reach/router";
 
 const useGetPath = () => {
     const location = useLocation();
-    const path = location.pathname.match(/\w+/gm);
-    return path.map(link => link[0].toUpperCase() + link.slice(1));
+    const path = location.pathname.replace(/-/g, " ").slice(1);
+    return path.split("/").map(link => link[0].toUpperCase() + link.slice(1));
 };
 
 export default useGetPath;
