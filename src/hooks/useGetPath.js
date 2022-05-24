9966051bc7ed49c036946bfc3f851427e5e94cad
Pavel Alloyarov <74268211+allpavel@users.noreply.github.com>
2022-05-24 21:25:25 +0300

    Update useGetPath.js

--- src/hooks/useGetPath.js
+++ src/hooks/useGetPath.js
@@ -3,7 +3,8 @@ import { useLocation } from "@reach/router";
 const useGetPath = () => {
     const location = useLocation();
     const path = location.pathname.replace(/-/g, " ").slice(1);
-    return path.split("/").map(link => link[0].toUpperCase() + link.slice(1));
+    // return path.split("/").map(link => link[0].toUpperCase() + link.slice(1));
+     return path.split("/");
 };
 
 export default useGetPath;
