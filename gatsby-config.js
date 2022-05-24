160afae0dc20b023596095e3769355d4598ca7e8
allpavel <allpavel@yandex.ru>
2022-05-24 21:52:47 +0300

    changed gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,12 @@ module.exports = {
         author: "Pavel Alloyarov",
     },
     plugins: [
+        {
+            resolve: `gatsby-plugin-styled-components`,
+            options: {
+                // Add any options here
+            },
+        },
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
