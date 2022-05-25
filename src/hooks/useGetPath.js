39320591f6c2a30dd85c95744153db98543866dc
allpavel <allpavel@yandex.ru>
2022-05-25 16:53:55 +0300

    small fixes in the useGetPath hook

--- src/hooks/useGetPath.js
+++ src/hooks/useGetPath.js
@@ -2,8 +2,10 @@ import { useLocation } from "@reach/router";
 
 const useGetPath = () => {
     const location = useLocation();
-    const path = location.pathname.replace(/-/g, " ").slice(1, -1).split("/");
+    const locationReplaceSlug = location.pathname.replace(/-/g, " ");
+    const path = locationReplaceSlug.replace(/(^\/+|\/+$)/gm, "").split("/");
     const breadcrumbs = path.map(pathItem => pathItem.charAt(0).toUpperCase() + pathItem.slice(1));
+
     return breadcrumbs;
 };
 
